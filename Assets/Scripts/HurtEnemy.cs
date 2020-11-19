eec44b009e0bf7323851e20b3ae2081296d6e5a1
Blossom13 <511643@chrome.escg.ac.uk>
2020-11-19 16:08:36 +0000

    Health for enemies and players, sword animations (only a really bad stab) and damage numbers for attacking

--- Assets/Scripts/HurtEnemy.cs
+++ Assets/Scripts/HurtEnemy.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class HurtEnemy : MonoBehaviour
 {
+
+    public int damageToGive;
+    public GameObject damageBurst;
+    public Transform hitPoint;
+    public GameObject damageNumber;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +26,13 @@ public class HurtEnemy : MonoBehaviour
     {
         if (other.gameObject.tag == "Enemy")
         {
-            Destroy(other.gameObject);
+            //Destroy(other.gameObject);
+            other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
+
+            Instantiate(damageBurst, hitPoint.position, hitPoint.rotation);
+
+            var clone = (GameObject) Instantiate(damageNumber, hitPoint.position, Quaternion.Euler(Vector3.zero));
+            clone.GetComponent<FloatingNumbers>().damageNumber = damageToGive;
         }
     }
 }
--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -81,12 +81,12 @@ public class SlimeController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.name == "Player")
+        /*if (other.gameObject.name == "Player")
         {
             //Destroy (other.gameObject);
             other.gameObject.SetActive(false);
             reloading = true;
             thePlayer = other.gameObject;
-        }
+        }*/
     }
 }

eaad6cb5112f56242d38ced70689a4ecd7796de1
DelphiFeverpitch <rawlinson322@gmail.com>
2020-11-25 13:02:03 +0000

    Bug fix and player movement script adjusted

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     private float currentMoveSpeed;
-    public float diagonalMoveModifier;
+    //public float diagonalMoveModifier;
 
     private Animator anim;
     private Rigidbody2D myRigidbody;
 
     private bool playerMoving;
     public Vector2 lastMove;
+    private Vector2 moveInput;
 
     private static bool playerExists;
 
@@ -37,6 +38,8 @@ public class PlayerController : MonoBehaviour
             Destroy(gameObject);
         }
 
+        lastMove = new Vector2(0, -1f);
+
     }
 
     // Update is called once per frame
@@ -48,30 +51,43 @@ public class PlayerController : MonoBehaviour
         if (!attacking)
         {
 
-            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            /*if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+              {
+                  //transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                  myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, myRigidbody.velocity.y);
+                  playerMoving = true;
+                  lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+              }
+
+              if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+              {
+                  //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                  myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * currentMoveSpeed);
+                  playerMoving = true;
+                  lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+              }
+
+              if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+              {
+                  myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+              }
+
+              if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+              {
+                  myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+              }*/
+
+            moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+            if(moveInput != Vector2.zero)
             {
-                //transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-                myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, myRigidbody.velocity.y);
+                myRigidbody.velocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
                 playerMoving = true;
-                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+                lastMove = moveInput;
             }
-
-            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            else
             {
-                //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * currentMoveSpeed);
-                playerMoving = true;
-                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
-            }
-
-            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
-            {
-                myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
-            }
-
-            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
-            {
-                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+                myRigidbody.velocity = Vector2.zero;
             }
 
             if (Input.GetKeyDown(KeyCode.J))
@@ -82,13 +98,13 @@ public class PlayerController : MonoBehaviour
                 anim.SetBool("Attack", true);
             }
         }
-            if(Mathf.Abs (Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs (Input.GetAxisRaw("Vertical")) > 0.5f)
+            /*if(Mathf.Abs (Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs (Input.GetAxisRaw("Vertical")) > 0.5f)
             {
                 currentMoveSpeed = moveSpeed * diagonalMoveModifier;
             } else
             {
                 currentMoveSpeed = moveSpeed;
-            }
+            }*/
 
             if (attackTimeCounter > 0)
             {

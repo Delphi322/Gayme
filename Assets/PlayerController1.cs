b0d194f9dc80151f632f0d335bb20afed6763dc7
Blossom13 <511643@chrome.escg.ac.uk>
2020-11-27 14:43:23 +0000

    this is hard

---
+++ Assets/PlayerController1.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public float moveSpeed;
+    private float currentMoveSpeed;
+
+    private Animator anim;
+    private Rigidbody2D myRigidbody;
+
+    private bool playerMoving;
+    public Vector2 lastMove;
+    private Vector2 moveInput;
+
+    private static bool playerExists;
+
+    private bool attacking;
+    public float attackTime;
+    private float attackTimeCounter;
+
+    public string startPoint;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        if (!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        lastMove = new Vector2(0, -1f);
+
+    }
+
+    void Update()
+    {
+
+
+        playerMoving = false;
+        if (!attacking)
+        {
+
+            moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+            if (moveInput != Vector2.zero)
+            {
+                myRigidbody.velocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+                playerMoving = true;
+                lastMove = moveInput;
+            }
+            else
+            {
+                myRigidbody.velocity = Vector2.zero;
+            }
+
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                attackTimeCounter = attackTime;
+                attacking = true;
+                myRigidbody.velocity = Vector2.zero;
+                anim.SetBool("Attack", true);
+            }
+        }
+        if (attackTimeCounter > 0)
+        {
+            attackTimeCounter -= Time.deltaTime;
+        }
+
+        if (attackTimeCounter <= 0)
+        {
+            attacking = false;
+            anim.SetBool("Attack", false);
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+
+    }
+}
